a65a5a5b0b96a12a8d0a687dfb153edfa32746dc
Anna Yuan <annayuan@Annas-MBP.fios-router.home>
2020-04-28 21:41:34 -0400

    Updates

---
+++ precache-manifest.f2dffc961fe3a762ef8f0b4f32e43a01.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8507cf4e2ca73718c3b99e1907fd5d68",
+    "url": "/ores-viz/index.html"
+  },
+  {
+    "revision": "da41cf6be9460ba5f086",
+    "url": "/ores-viz/static/css/main.9d06bb23.chunk.css"
+  },
+  {
+    "revision": "adeb6f9492efe22ef7e2",
+    "url": "/ores-viz/static/js/2.a0130761.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/ores-viz/static/js/2.a0130761.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da41cf6be9460ba5f086",
+    "url": "/ores-viz/static/js/main.63a1cb2d.chunk.js"
+  },
+  {
+    "revision": "b92c54c423b9b71db106",
+    "url": "/ores-viz/static/js/runtime-main.425a40d9.js"
+  },
+  {
+    "revision": "88e6b8936ee9787977b07b9d0b5b81ec",
+    "url": "/ores-viz/static/media/data_balanced.88e6b893.csv"
+  },
+  {
+    "revision": "88e6b8936ee9787977b07b9d0b5b81ec",
+    "url": "/ores-viz/static/media/data_group.88e6b893.csv"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ores-viz/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "760a321c9bc7b69fd1f6725fe0c11fd0",
+    "url": "/ores-viz/static/media/performance.760a321c.csv"
+  }
+]);
