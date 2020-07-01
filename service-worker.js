2ae788c7e5c3dcb379846ccf92e114311a1adc0b
ShaurGaur <shauryavrat@live.com>
2020-07-01 15:34:41 -0700

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/ores-viz/precache-manifest.e137c9714e2cab679399f7457c656cd9.js"
+  "/ores-viz/precache-manifest.060d80046ca0bc4d9dce2db45fb7db5d.js"
 );
 
 self.addEventListener('message', (event) => {
